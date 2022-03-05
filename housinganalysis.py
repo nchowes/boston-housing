a5cbbdc05918870b94fcdf6d8c4b89a2b161ed50
nchowes <nchowes@gmail.com>
2022-03-05 12:14:21 -0500

    update app, org repo

--- housinganalysis.py
+++ housinganalysis.py
@@ -1,9 +1,17 @@
-#housinganalysis.py
+"""
+    housinganalysis
+    ---------------
+
+    This module contains utilities used in the boston housing market analysis. 
+
+"""
+
+import os 
 import requests
 import pandas as pd
 
 
-def pulldata( year, url ):
+def pulldata( year, url, path_to_data=None ):
     """Get annual housing price data from html table in url."""
 
     statusOk = False
@@ -11,9 +19,11 @@ def pulldata( year, url ):
     html = requests.get(url).content
     df_list = pd.read_html(html)
     df = df_list[-1]
+    
     filename = 'housingData' + year + '.csv' 
+    if path_to_data:
+        filename = os.path.join(path_to_data, filename)
     df.to_csv( filename )
-
     print(filename)
 
     statusOk = True
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,27 +8,7 @@ import pandas as pd
 
 @st.cache
 def load_and_prepare():
-
-    # Read data 
-    df = pd.read_csv("housingData2013prepared.csv")
-
-    # Reorder 
-    df = df.reindex(columns=["Town", "pr2013", "pr2012", "pr2008", "pr2003", "pc1y", "pc5y", "pc10y", "dm2013", "dm2012", "dc1y"])
-
-    # Display names 
-    df = df.rename(columns={
-        "pr2013": "2013 Price",    
-        "pr2012": "2012 Price",   
-        "pr2008": "2008 Price",    
-        "pr2003": "2003 Price",    
-        "pc1y": "1Yr Change",      
-        "pc5y": "5Yr Change",     
-        "pc10y": "10Yr Change",       
-        "dm2013": "2013 Days on Market",  
-        "dm2012": "2012 Days on Market",  
-        "dc1y": "1Yr DOM Change",    
-    })
-
+    df = pd.read_csv("./data/product/housing-data-y19y21price-manual.csv")
     return df
 
 # Load data 
@@ -37,24 +17,30 @@ df = load_and_prepare()
 # Layout app 
 
 # Title
-st.title('Metro Boston housing market')
+st.title('Metro Boston Housing: 2019-2021')
 st.subheader('Single family home prices')
 
 # Table
 st.write(df)
 
-st.subheader('By Town')
+st.subheader('By City/Town')
 
 # Selectbox
 option = st.selectbox(
     'Which town would you like to see?',
-     df["Town"])
+     df["City/Town"])
 
 #By Town
-df2 = df.set_index("Town", inplace = False)
+df2 = df.set_index("City/Town", inplace = False)
+
+st.text('Housing price')
 
 st.bar_chart( 
-    df2.loc[[option],["2013 Price", "2012 Price", "2008 Price", "2003 Price"]].T[option]
+    df2.loc[[option],["2021 Price", "2020 Price", "2019 Price"]].T[option]
      )
 
+st.text('Housing price percent change')
 
+st.bar_chart( 
+    df2.loc[[option],["2Yr %Change ('19-'21)", "1Yr %Change ('20-'21)", "1Yr %Change ('19-'20)"]].T[option]
+     )
